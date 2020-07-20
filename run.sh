48c35898659080955200849514e8a21d8f5677f9
Sean Law <seanmylaw@gmail.com>
2020-07-20 10:00:59 -0400

    Updated server name

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 APPDIR="$( cd "$( dirname "$0" )" && pwd )"
 STUMPY_STATS=`$APPDIR/get_stumpy_growth.py 2> /dev/null`
+HOSTNAME=`uname -n`
 
-curl -k -X POST -H 'Content-type: application/json' --data "{\"text\":\"$STUMPY_STATS\"}" https://hooks.slack.com/services/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 -o /dev/null
+curl -k -X POST -H 'Content-type: application/json' --data "{\"text\":\"Server: $HOSTNAME\n$STUMPY_STATS\"}" https://hooks.slack.com/services/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 -o /dev/null
