65a1fec2e36012c70e66e24ea7eb0cbbc552b9e1
Sean Law <seanmylaw@gmail.com>
2020-07-20 09:33:35 -0400

    Added slack webhook script

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+APPDIR="$( cd "$( dirname "$0" )" && pwd )"
+STUMPY_STATS=`$APPDIR/get_stumpy_growth.py 2> /dev/null`
+
+curl -k -X POST -H 'Content-type: application/json' --data "{\"text\":\"$STUMPY_STATS\"}" https://hooks.slack.com/services/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 -o /dev/null
