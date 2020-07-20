57d1b9de566e6f43901ccd85ad408a73ea9a9867
Sean Law <seanmylaw@gmail.com>
2020-07-20 11:09:37 -0400

    Added more notes to README for running slack webhook

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ Install dependencies
 ```
 ./setup.sh
 ```
+
+# Setting Up Cron Job
+
+1. Update the `PYTHON` variable `run.sh` script so that it points to your local python executable
+2. Add `0 9 * * * /path/to/stumpy_growth.git/run.sh` to your cron jobs
+
+# Slack Webhook
+
+To find the Webhook URL, visit `api.slack.com`, select your app, and click on "Install App" under "Settings" along the left-hand-side menu. In the new screen, you should see the "Webhook URL" near the bottom of your screen.
--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-APPDIR="$( cd "$( dirname "$0" )" && pwd )"
-STUMPY_STATS=`$APPDIR/get_stumpy_growth.py 2> /dev/null`
+PYTHON="/home/law978/miniconda3/bin/python"
 HOSTNAME=`uname -n`
+APPDIR="$( cd "$( dirname "$0" )" && pwd )"
+STUMPY_STATS=`$PYTHON $APPDIR/get_stumpy_growth.py 2> /dev/null`
 
-curl -k -X POST -H 'Content-type: application/json' --data "{\"text\":\"Server: $HOSTNAME\n$STUMPY_STATS\"}" https://hooks.slack.com/services/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 -o /dev/null
+curl -k -X POST -H 'Content-type: application/json' --data "{\"text\":\"Server: $HOSTNAME\n$STUMPY_STATS\nend\"}" https://hooks.slack.com/services/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 -o /dev/null
