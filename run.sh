d62c0b5e921f33dae35cc045ee1fd746c10df373
Sean Law <seanmylaw@gmail.com>
2020-07-25 17:25:54 -0400

    Fixed miising closing double quote

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ HOSTNAME=`uname -n`
 APPDIR="$( cd "$( dirname "$0" )" && pwd )"
 STUMPY_STATS=`$PYTHON $APPDIR/get_stumpy_growth.py 2> /dev/null`
 
-curl -k -X POST -H 'Content-type: application/json' --data "{\"text\":\"Server: $HOSTNAME\n$STUMPY_STATS\n"}" https://hooks.slack.com/services/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 -o /dev/null
+curl -k -X POST -H 'Content-type: application/json' --data "{\"text\":\"Server: $HOSTNAME\n$STUMPY_STATS\n\"}" https://hooks.slack.com/services/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 -o /dev/null
