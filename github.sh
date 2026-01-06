553a403bdd1e1b414ebe1c148a0aa860bf87b029
Sean Law <seanmylaw@gmail.com>
2026-01-06 10:03:04 -0500

    Test

--- github.sh
+++ github.sh
@@ -2,7 +2,7 @@
 
 SLACK_WEBHOOK="$1"
 HOSTNAME="Github Actions"
-STUMPY_STATS=`python3 get_stumpy_growth.py 2> /dev/null`
-# echo $STUMPY_STATS
+STUMPY_STATS=`python3 get_stumpy_growth.py`
+#echo $STUMPY_STATS
 
 curl -k -X POST -H 'Content-type: application/json' --data "{\"text\":\"Server: $HOSTNAME\n$STUMPY_STATS\n\"}" $SLACK_WEBHOOK -o /dev/null
